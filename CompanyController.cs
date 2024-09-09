d58edd13100302d54a4a0dc78c252a687fd7e34c
Stefano Hämmerle <shm@orgadata.ag>
2024-09-09 15:59:57 +0200

    Projektdateien hinzufügen.

---
+++ CompanyController.cs
@@ -0,0 +1,79 @@
+﻿using ERPDemoAPI.Models;
+using ERPDemoAPI.Models.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ERPDemoAPI;
+[Route("api/[controller]")]
+[ApiController]
+public class CompanyController : ControllerBase
+{
+    private readonly ERPDemoAPIContext _context;
+
+    public CompanyController(ERPDemoAPIContext context)
+    {
+        _context = context; 
+    }
+
+    // GET: api/<ValuesController>
+    [HttpGet]
+    public List<Company> Get()
+    {
+        return _context.Companies.OrderBy(n => n.Id).AsNoTracking().ToList();
+    }
+
+    // GET api/<ValuesController>/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return "value";
+    }
+
+    // GET api/<ValuesController>/5
+    [HttpGet("ByCompanyName/{name}")]
+    public List<Company> SearchByName(string name)
+    {
+        return _context.Companies.Where(n => n.Firma.Contains(name)).OrderBy(n => n.Id).AsNoTracking().ToList();
+    }
+
+    // GET api/<ValuesController>/city/5
+    [HttpGet("ByCity/{city}")]
+    public List<Company> GetCompaniesByCity(string city)
+    {
+        return _context.Companies.Where(n => n.Ort == city).OrderBy(n => n.Id).AsNoTracking().ToList();
+    }
+
+    // GET api/<ValuesController>/city/5
+    [HttpGet("ByCountry/{country}")]
+    public List<Company> GetCompaniesByCountry(string country)
+    {
+        return _context.Companies.Where(n => n.Land == country).OrderBy(n => n.Id).AsNoTracking().ToList();
+    }
+
+    // GET api/<ValuesController>/city/5
+    [HttpGet("ByCountryCode/{countryCode}")]
+    public List<Company> GetCompaniesByCountryCode(string countryCode)
+    {
+        return _context.Companies.Where(n => n.LandIsocode == countryCode).OrderBy(n => n.Id).AsNoTracking().ToList();
+    }
+
+    // POST api/<ValuesController>
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/<ValuesController>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/<ValuesController>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
